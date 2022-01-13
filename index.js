d593352998d4705c3890bef93449ae6adb0b7d08
toshetasevski <toshetasevski@Toshes-MacBook-Pro.local>
2022-01-13 15:03:49 -0500

    template created

--- index.js
+++ index.js
@@ -43,13 +43,15 @@ inquirer.prompt([
         name: 'email',
         message: 'Please enter managaers email!',
         validate: emailInput => {
-            if (emailInput) {
-                return true
-            } else {
-                console.log('Please enter managers email!')
+            if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(emailInput)) {
+                return (true)
+            }else{
+            console.log("Invalid email address!")
+            return (false)
             }
         }
     },
+    
     {
         type: 'input',
         name: 'officeNumber',
@@ -119,14 +121,14 @@ const addEngineer = () => {
             type: 'input',
             name: 'id',
             message: 'Please enter engineer ID!',
-            // validate: idInput => {
-            //     if (!isNaN(idInput)) {
-            //         console.log('Please enter engineers Id!')
-            //         return false
-            //     } else {
-            //         return true;
-            //     }
-            // }
+            validate: idInput => {
+                if (isNaN(idInput)) {
+                    console.log('Please enter engineers Id!')
+                    return false
+                } else {
+                    return true;
+                }
+            }
         },
         {
             type: 'input',
@@ -143,7 +145,7 @@ const addEngineer = () => {
 
         {
             type: 'input',
-            name: 'github',
+            name: 'gitHub',
             message: 'Please enter engineers gitHub username',
             validate: gitHubInput => {
                 if (gitHubInput) {
@@ -168,6 +170,45 @@ function addIntern() {
     inquirer.prompt([
         {
             type: 'input',
+            name: 'name',
+            message: ('Inntern name?'),
+            validate: nameInput => {
+                if (nameInput) {
+                    return true
+                } else {
+                    console.log('Please enter Intern name!')
+                    return false
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'Please enter Intern ID!',
+            validate: idInput => {
+                if (isNaN(idInput)) {
+                    console.log('Please enter Intern Id!')
+                    return false
+                } else {
+                    return true;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Please enter Intern email!',
+            validate: emailInput => {
+                if (emailInput) {
+                    return true
+                } else {
+                    console.log('Please enter Intern email!')
+                }
+            }
+        },
+
+        {
+            type: 'input',
             name: 'school',
             message: 'Please enter intern school',
             validate: schoolInput => {
@@ -182,12 +223,18 @@ function addIntern() {
             type: 'confirm',
             name: 'confirmAddEmployee',
             message: 'Would you like to add more employees?',
-            default: false
+            // default: false
         },
-        
-        
-
     ])
+        .then(internInput => {
+            const intern = new Intern(internInput.name, internInput.Id, internInput.email)
+
+            workersArray.push(intern);
+            console.log(workersArray);
+            addEmployee()
+
+
+        })
 }
 
 function createTeam() {
---
+++ src/template.js
@@ -0,0 +1,62 @@
+function createTeam(team) {
+    function createManager(manager) {
+        return `
+        <div>
+            <h2>Name: ${manager.getName()}</h2>
+            <h5>Role: ${manager.getRole()}</h5>
+            <h5>ID: ${manager.getId()}</h5>
+            <h5>Email:<a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></h5>
+            <h5>Office Number: ${manager.getOfficeNumber()}</h5>
+        
+        </div>
+        `
+    }
+
+    function createEngineer(engineer) {
+        return `
+        <div>
+            <h2>Name:${engineer.getName()}</h2>
+            <h5>Role: ${engineer.getRole()}</h5>
+            <h5>ID: ${engineer.getId()}</h5>
+            <h5>Email:<a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></P>
+            <h5>GitHub Username: ${engineer.getGithub()}</h5>
+            </div>
+        `
+    }
+
+    function createIntern(intern) {
+        return `
+        <div>
+            <h2>Name: ${intern.getName()}</h5>
+            <h5>Id: ${intern.getId()}</h5>
+            <h5>Email: ${intern.getEmail()}</h5>
+            <h5>School: ${intern.getSchool()}</h5>
+            <h5>Role: ${intern.getRole()}</h5>
+        </div>
+        `
+
+    }
+
+    const workers = []
+    // pushing team members into the array and checking their roles to run the correct function
+    workers.push(team.filter(worker => worker.getRole() === "Manager").map(manager => createManager(manager)))
+    workers.push(team.filter(worker => worker.getRole() === "Engineer").map(engineer => createEngineer(engineer)))
+    workers.push(team.filter(worker => worker.getRole() === "Intern").map(intern => createIntern(intern)))
+    return workers.join("")
+
+}
+
+module.exports = team => {
+    return `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <div>${createTeam(team)}</div>
+    </body>
+    </html>`
+}
