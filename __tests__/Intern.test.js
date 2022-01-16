3097a22d984c30015bd2132eaae3f2d4b29a7485
toshetasevski <toshetasevski@Toshes-MacBook-Pro.local>
2022-01-16 10:07:33 -0500

    intern test

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,3 +1,21 @@
+const Intern = require('../lib/Employee')
+
+test("Is this an object?", () => {
+    const obj = new Intern ()
+    expect(typeof(obj)).toBe('object')
+})
+
+test ("Can we get an email?", () => {
+    const email = 'test'
+    const intern = new Intern('name', 'id', email)
+    expect(intern.getEmail()).toBe(email)
+})
+
+test ("Can we get a school?", () => {
+    const school = 'test'
+    const intern = new Intern('name', 'id', 'email', school)
+    expect(intern.schooll).toBe(school)
+})
 
 
 
@@ -19,6 +37,3 @@
 
 
 
-
-
-// school
--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,1 +1,1 @@
-// officenumber
+
